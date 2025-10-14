f3bc9164c6a17fd72c0109accedc8f33e4bf4016
sangeeth <sangeeth99614@gmail.com>
2025-10-14 17:53:41 +0530

    setup backend folder structure and connect to database

---
+++ server/server.js
@@ -0,0 +1,27 @@
+
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/db.js'
+
+const app = express()
+
+// Connect to Database
+await connectDB();
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => {
+    res.send("Hello from QuickGPT server")
+})
+
+// Start the server
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
